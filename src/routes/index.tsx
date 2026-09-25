245a16c058d73ee8316a6f456ff73045b8fe124f
sathwikre <sathwikreddy0303@gmail.com>
2026-09-25 13:37:32 +0530

    updated the in fileds

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2407,7 +2407,7 @@ function OmSolutionsHome() {
                         </p>
                       </div>
                     </div>
-                    <div className="border-t border-border/70 px-4 py-3">
+                    <div className="border-t border-border/70 px-4 py-3 text-center">
                       <p className="text-sm font-semibold text-foreground">{member.name}</p>
                     </div>
                   </button>
